7c3eaff291f2a7c720aa1fbb0163d997582dd540
Dev-hottae <hottae0805@gmail.com>
2020-03-26 16:22:24 +0900

    1st complete 0.0.1
    now run - 0.0.1

--- _main.py
+++ _main.py
@@ -1,12 +1,14 @@
-import sys
-import os
-import jwt
+import hashlib
+import json
+import time
+import uuid
 from urllib.parse import urlencode
-from account import keys
-from account.login import *
-from scheduler_test.getData import *
-from market_data.market_data import *
+
+import jwt
+import requests
 from apscheduler.schedulers.background import BackgroundScheduler
+
+from account import keys
 
 ## 기본 변수설정
 # account 관련변수
@@ -17,10 +19,20 @@ market = ["KRW-BTC", "KRW-ETH"]
 current_price_url = "https://api.upbit.com/v1/ticker"
 prev_price_url = "https://api.upbit.com/v1/candles/days"
 
+# 전일 데이터를 가져오기 위한 input data
+get_url = "https://api.upbit.com/v1/candles/days"
+querystring_BTC = {"market": "KRW-BTC", "count": "2", "convertingPriceUnit": "KRW"}
+querystring_ETH = {"market": "KRW-ETH", "count": "2", "convertingPriceUnit": "KRW"}
+
+
 global prev_btc_data
 global prev_eth_data
 global target_btc
 global target_eth
+global btc_current_price
+btc_current_price = 0
+global eth_current_price
+eth_current_price = 0
 money_for_btc = 0
 money_for_eth = 0
 
@@ -43,62 +55,67 @@ def Main():
     sched = BackgroundScheduler()
     sched.start()
 
-
-    #오더
+    # 테스트 오더
     order_btc_for_waiting()
     print("대기용 매수주문완료")
-    ## 실제 실행
-    # 9시 정각 모든 자산 매도주문 & 걸린 주문들 전체 취소
-    sched.add_job(request_sell, 'interval', seconds=5, id="sell_having_asset")
-    sched.add_job(waits_order_cancel, 'interval', seconds=5, id="order_cancel")
+    order_btc_for_done()
+    print("매도용 매수주문완료")
 
+    # ## 실제 실행
+    # # 9시 정각 모든 자산 매도주문 & 걸린 주문들 전체 취소 & 계좌데이터 refresh & 전일 데이터로 타겟 설정
+    sched.add_job(morning_9am, 'cron', hour=9, id="morning_9am")
 
-
-    # My account 정보 호출 // 잔고 변화 이벤트 있을 시 수시 호출 가능하게 변경할 것
-
-    # account_data = account_info()
-    #
-    # my_balance = account_data.json()[0].get('balance')
-    # my_balance = str(round(float(my_balance), 3))
-    #
-    # # 매매를 위한 금액설정
-    # money_for_btc = float(my_balance) * (49.5 / 100)
-    # money_for_eth = float(my_balance) * (49.5 / 100)
-    #
-    # main_currency = account_data.json()[0].get('currency')
-    # print("현재 My account Balance : " + my_balance + main_currency)
-    #
-    #
-    # # 시장 현재가 데이터 호출(쓰레드 분리)
-    # app = QApplication(sys.argv)
-    # # price_helper = Market_datas(market, current_price_url)
-    # # price_helper.finished.connect(price_helper.update_marketdata)
-    # # price_helper.start()
-    #
-    #
-    # # 스케쥴 테스트용
-    # sched.add_job(get_btc, 'interval', seconds=6, id="get_btc")
-    # sched.add_job(get_eth, 'interval', seconds=6, id="get_eth")
-    # sched.add_job(get_current_btc, 'interval', seconds=3, id="get_cur_btc")
-    # sched.add_job(get_current_eth, 'interval', seconds=3, id="get_cur_eth")
+    # # 현재 데이터 지속적으로 받아오기
+    sched.add_job(get_current_btc, 'interval', seconds=2, id="get_cur_btc")
+    sched.add_job(get_current_eth, 'interval', seconds=2, id="get_cur_eth")
 
     while True:
         print("now running")
-        time.sleep(5)
+        global btc_current_price
+        global eth_current_price
+        print("btc 현재가 : ", btc_current_price)
+        print("eth 현재가 : ", eth_current_price)
+        if btc_current_price >= get_target_price_btc(get_btc()):
+            order_btc()
 
-    # 스케쥴 매9시 실행
-    # sched.add_job(get_btc, 'cron', hour=9, id="get_btc")
-    # sched.add_job(get_eth, 'cron', hour=9, id="get_etc")
+        if eth_current_price >= get_target_price_eth(get_eth()):
+            order_eth()
 
-    # app.exec_()
+        time.sleep(1)
 
     return
 
-# 전일 데이터를 가져오기 위한 input data
-get_url = "https://api.upbit.com/v1/candles/days"
-querystring_BTC = {"market": "KRW-BTC", "count": "2", "convertingPriceUnit": "KRW"}
-querystring_ETH = {"market": "KRW-ETH", "count": "2", "convertingPriceUnit": "KRW"}
 
+# 9시에 실행될 함수
+def morning_9am():
+    print("현재 시각 오전 9시@@@@")
+    print("현재 주문 내역 : ", ordered_uuids)
+    request_sell()
+    waits_order_cancel()
+
+    ordered_uuids.clear()
+    print("-----전일 주문 취소 완료!!-----")
+
+    account_data = account_info()
+
+    my_balance = account_data.json()[0].get('balance')
+    my_balance = str(round(float(my_balance), 3))
+
+    # 매매를 위한 금액설정
+    global money_for_btc
+    global money_for_eth
+    money_for_btc = float(my_balance) * (49.5 / 100)
+    money_for_eth = float(my_balance) * (49.5 / 100)
+
+    main_currency = account_data.json()[0].get('currency')
+    print("금일 현재 My account Balance : " + my_balance + main_currency)
+
+    print("-----금일 계좌 데이터 초기화 완료!!!-----")
+
+    get_btc()
+    get_eth()
+
+    print("-----전일 데이터 요청 완료!!-----")
 
 # 계정 데이터 관련 함수
 def account_info():
@@ -116,7 +133,6 @@ def account_info():
     headers = {"Authorization": authorize_token}
     res = requests.get(server_url + "/v1/accounts", headers=headers)
     return res
-
 
 # 전일 데이터 호출함수
 def get_btc():
@@ -143,31 +159,19 @@ def get_current_btc():
     url = "https://api.upbit.com/v1/ticker"
     querystring = {"markets": "KRW-BTC"}
     response = json.loads(requests.request("GET", url, params=querystring).text)
-    global btc_trade_price
-    btc_trade_price = response[0]["trade_price"]
+    global btc_current_price
+    btc_current_price = response[0]["trade_price"]
 
-    if btc_trade_price >= get_target_price_btc(get_btc()):
-        order_btc()
-
-    print("현재 btc 타겟 price : %s" %get_target_price_btc(get_btc()))
-    print("currnet btc price : " + str(response[0]["trade_price"]))
-    print("BTC 매수불가")
-    return btc_trade_price
+    return btc_current_price
 
 def get_current_eth():
     url = "https://api.upbit.com/v1/ticker"
     querystring = {"markets": "KRW-ETH"}
     response = json.loads(requests.request("GET", url, params=querystring).text)
-    global eth_trade_price
-    eth_trade_price = response[0]["trade_price"]
+    global eth_current_price
+    eth_current_price = response[0]["trade_price"]
 
-    if eth_trade_price >= get_target_price_eth(get_eth()):
-        order_eth()
-
-    print("현재 eth 타겟 price : %s" %get_target_price_eth(get_eth()))
-    print("currnet eth price : " + str(response[0]["trade_price"]))
-    print("ETH 매수불가")
-    # return response
+    return eth_current_price
 
 
 # 목표 매수가 돌파 시 주문 호출을 위한 함수
@@ -270,14 +274,15 @@ def get_target_price_eth(coin_price):
 ### 오전9시 보유종목 매도 및 전체 등록 주문 취소
 def waits_order_cancel():
     # 현재 대기열에 있는 주문들 uuid 값들을 가져옴
-    wait_uuids = order_uuids("wait")
+    wait_uuids = order_uuids('wait')
+    print("대기중인 uuid : ", wait_uuids)
     if len(wait_uuids)==0:
         print("현재 waiting 주문의 건이 없습니다...")
         return
     print("총 %s 건의 주문이 waiting 중입니다...." % len(wait_uuids))
     for i in range(len(wait_uuids)):
         order_cancel(wait_uuids[i])
-
+        # ordered_uuids.remove(wait_uuids[i])  # 취소 후 uuid 제거
     print("waiting 주문들의 취소가 정상 처리되었습니다...")
 
 def order_cancel(id):
@@ -306,7 +311,7 @@ def order_cancel(id):
 # 현재 대기열에 있는 주문 uuid 값들을 가져옴
 def order_uuids(situation):
     query = {
-        'state': situation,
+        'state': situation
     }
     query_string = urlencode(query)
 
@@ -341,7 +346,8 @@ def order_uuids(situation):
 # 오전 9시 현재 보유자산 처분 요청
 def request_sell():
     # 현재 주문 완료된 uuid 값들을 가져옴
-    done_uuids = order_uuids("done")
+    done_uuids = order_uuids('done')
+    print("매도해야할 uuid: ", done_uuids)
     if len(done_uuids)==0:
         print("현재 매수완료된 주문의 건이 없습니다...")
         return
@@ -349,6 +355,7 @@ def request_sell():
 
     for i in range(len(done_uuids)):
         sell_asset(done_uuids[i])
+        # ordered_uuids.remove(done_uuids[i])     # 매도 후 uuid 제거
     print("모든 보유자산의 매도가 정상 처리되었습니다....")
 
 
@@ -358,8 +365,7 @@ def sell_asset(id):
         'market': id['market'],
         'side': 'ask',
         'volume': id['volume'],
-        'price': str(get_current_btc()),  # 전일 close price 가 가장 좋음(바꿀것)
-        'ord_type': 'limit',
+        'ord_type': 'market',
     }
     query_string = urlencode(query).encode()
 
@@ -389,37 +395,77 @@ def sell_asset(id):
 
 ### 테스트용 오더
 
-# def order_btc_for_waiting():
-#     print('btc 주문실행...')
-#     access_key = keys.access_key
-#     secret_key = keys.secret_key
-#     server_url = 'https://api.upbit.com'
-#
-#     query = {
-#         'market': 'KRW-BTC',
-#         'side': 'bid',
-#         'volume': '0.00116928',
-#         'price': '900000',
-#         'ord_type': 'limit',
-#     }
-#     query_string = urlencode(query).encode()
-#
-#     m = hashlib.sha512()
-#     m.update(query_string)
-#     query_hash = m.hexdigest()
-#
-#     payload = {
-#         'access_key': access_key,
-#         'nonce': str(uuid.uuid4()),
-#         'query_hash': query_hash,
-#         'query_hash_alg': 'SHA512',
-#     }
-#
-#     jwt_token = jwt.encode(payload, secret_key).decode('utf-8')
-#     authorize_token = 'Bearer {}'.format(jwt_token)
-#     headers = {"Authorization": authorize_token}
-#
-#     res = requests.post(server_url + "/v1/orders", params=query, headers=headers)
-#     res = res.json()
-#     ordered_uuids.append(res["uuid"])
+def order_btc_for_waiting():
+    print('btc 주문실행...')
+    access_key = keys.access_key
+    secret_key = keys.secret_key
+    server_url = 'https://api.upbit.com'
+
+    query = {
+        'market': 'KRW-BTC',
+        'side': 'bid',
+        'volume': '0.00015714',
+        'price': '7000000',
+        'ord_type': 'limit',
+    }
+    query_string = urlencode(query).encode()
+
+    m = hashlib.sha512()
+    m.update(query_string)
+    query_hash = m.hexdigest()
+
+    payload = {
+        'access_key': access_key,
+        'nonce': str(uuid.uuid4()),
+        'query_hash': query_hash,
+        'query_hash_alg': 'SHA512',
+    }
+
+    jwt_token = jwt.encode(payload, secret_key).decode('utf-8')
+    authorize_token = 'Bearer {}'.format(jwt_token)
+    headers = {"Authorization": authorize_token}
+
+    res = requests.post(server_url + "/v1/orders", params=query, headers=headers)
+    res = res.json()
+    time.sleep(3)
+    print("uuid 찾기 위한 프린트 : ", res)
+    ordered_uuids.append(res["uuid"])
+
+
+
+def order_btc_for_done():
+    print('btc 주문실행...')
+    access_key = keys.access_key
+    secret_key = keys.secret_key
+    server_url = 'https://api.upbit.com'
+
+    query = {
+        'market': 'KRW-BTC',
+        'side': 'bid',
+        'volume': '0.00013415',
+        'price': '8300000',
+        'ord_type': 'limit',
+    }
+    query_string = urlencode(query).encode()
+
+    m = hashlib.sha512()
+    m.update(query_string)
+    query_hash = m.hexdigest()
+
+    payload = {
+        'access_key': access_key,
+        'nonce': str(uuid.uuid4()),
+        'query_hash': query_hash,
+        'query_hash_alg': 'SHA512',
+    }
+
+    jwt_token = jwt.encode(payload, secret_key).decode('utf-8')
+    authorize_token = 'Bearer {}'.format(jwt_token)
+    headers = {"Authorization": authorize_token}
+
+    res = requests.post(server_url + "/v1/orders", params=query, headers=headers)
+    res = res.json()
+    time.sleep(3)
+    print("uuid 찾기 위한 프린트 : ", res)
+    ordered_uuids.append(res["uuid"])
 
