fdbbb7bcdb5bda7e1597a5536caf2e4fc51c2a84
Dev-hottae <hottae0805@gmail.com>
2020-04-03 09:08:58 +0900

    update

--- _main.py
+++ _main.py
@@ -68,7 +68,7 @@ def Main():
 
     # ## 실제 실행
     # # 9시 정각 모든 자산 매도주문 & 걸린 주문들 전체 취소 & 계좌데이터 refresh & 전일 데이터로 타겟 설정
-    sched.add_job(morning_9am, 'cron', hour=9, id="morning_9am")
+    sched.add_job(morning_9am, 'cron', hour=9, minute=10, id="morning_9am")
 
     # # 현재 데이터 지속적으로 받아오기
     sched.add_job(get_current_btc, 'interval', seconds=2, id="get_cur_btc")
@@ -101,7 +101,7 @@ def Main():
             money_for_eth = 0
 
         time.sleep(1)
-    return
+
 
 
 # 9시에 실행될 함수
--- testing.py
+++ testing.py
@@ -1,51 +1,18 @@
+# import time
 #
-# # 목표 매수가 돌파 시 주문 호출을 위한 함수
-# import hashlib
-# import uuid
-# from urllib.parse import urlencode
+# from apscheduler.schedulers.background import BackgroundScheduler
 #
-# import jwt
-# import requests
 #
-# from _main import get_target_price_btc, get_btc
-# from account import keys
+# def test():
+#     sched = BackgroundScheduler()
+#     sched.start()
 #
-# ordered_uuids = []
-# def order_btc():
-#     print('btc 주문실행...')
-#     access_key = keys.access_key
-#     secret_key = keys.secret_key
-#     server_url = 'https://api.upbit.com'
-#     global money_for_btc
-#     query = {
-#         'market': "KRW-BTC",
-#         'side': "bid",
-#         'volume': str(money_for_btc / get_target_price_btc(get_btc())),
-#         'price': str(get_target_price_btc(get_btc())),
-#         'ord_type': "limit",
-#     }
-#     query_string = urlencode(query).encode()
+#     sched.add_job(pri, 'cron', hour=9, minute=7, id="pri")
 #
-#     m = hashlib.sha512()
-#     m.update(query_string)
-#     query_hash = m.hexdigest()
+# def pri():
+#     print("running")
 #
-#     payload = {
-#         'access_key': access_key,
-#         'nonce': str(uuid.uuid4()),
-#         'query_hash': query_hash,
-#         'query_hash_alg': 'SHA512',
-#     }
 #
-#     jwt_token = jwt.encode(payload, secret_key).decode('utf-8')
-#     authorize_token = 'Bearer {}'.format(jwt_token)
-#     headers = {"Authorization": authorize_token}
-#
-#     res = requests.post(server_url + "/v1/orders", params=query, headers=headers)
-#     res = res.json()
-#     print("res : ", res)
-#     # 주문 날리면 uuid 저장
-#     ordered_uuids.append(res["uuid"])
-#     print(ordered_uuids)
-#
-# order_btc()
+# test()
+# while True :
+#     time.sleep(1)
