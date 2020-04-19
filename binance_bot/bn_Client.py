4156fd68d6bedf86a0960d0bc1924e25a19b8926
Dev-hottae <hottae0805@gmail.com>
2020-04-19 13:27:47 +0900

    account info 에 added print

--- binance_bot/bn_Client.py
+++ binance_bot/bn_Client.py
@@ -64,6 +64,7 @@ class Bn_Client():
         url = Bn_Client.HOST + endpoint
 
         res = requests.get(url, params=query, headers=header)
+        print(res.json())
         return res.json()
 
     # 현재 보유 잔고 확인
@@ -329,16 +330,3 @@ class Bn_Client():
         return target_price
 
 
-'''
-def servertime_checking():
-    int(time.time() * 1000) - client.get_server_time()['serverTime']
-    for i in range(1, 10):
-        local_time1 = int(time.time() * 1000)
-        server_time = client.get_server_time()
-        diff1 = server_time['serverTime'] - local_time1
-        local_time2 = int(time.time() * 1000)
-        diff2 = local_time2 - server_time['serverTime']
-        print("local1: %s server:%s local2: %s diff1:%s diff2:%s" % (
-            local_time1, server_time['serverTime'], local_time2, diff1, diff2))
-        time.sleep(2)
-'''
--- binance_bot/testing.py
+++ binance_bot/testing.py
@@ -15,3 +15,4 @@ from pytz import timezone
 from account.keys import *
 from binance_bot.bn_Client import Bn_Client
 from upbit_bot.ub_Client import Ub_Client
+
